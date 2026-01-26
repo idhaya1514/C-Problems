ac527381344d7a4d373bdb7a69471b4bf9bf6075
IdhayaKumar <idhayakumar1514@gmail.com>
2026-01-26 13:44:18 +0530

    Add files via upload
    Multiple of 3 and 7

---
+++ multiple of 3 and 7.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+   int n;
+   scanf("%d",&n);
+   if(n%3==0 && n%7==0)
+   {
+       printf("%d is divible by 3 and 7",n);
+   }
+   else
+   {
+       printf("%d is not divisible by 3 and 7",n);
+   }
+    return 0;
+}

ec803bacd332ec90a144af00ee14f01b2ac87d03
IdhayaKumar <idhayakumar1514@gmail.com>
2026-01-26 14:01:41 +0530

    Add files via upload
    Calculate the days in month

---
+++ calculate the days in month using switch case.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int Month;
+    scanf("%d",&Month);
+    switch(Month)
+    {
+        case 1:case 3:case 5:case 7:case 8:case 10:case 12 :
+            printf("31 Days");
+            break;
+        case 4: case 6 : case 9:case 11:
+            printf("30 Days");
+            break;
+        case 2 :
+            printf("28 or 29 Days");
+            break;
+        default:
+        printf("Invalid Month");
+    }
+    
+    return 0;
+}

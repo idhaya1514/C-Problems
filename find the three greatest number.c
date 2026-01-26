29c44858d39d2d7bd4809f8830142e7d65fdebd3
IdhayaKumar <idhayakumar1514@gmail.com>
2026-01-26 13:47:50 +0530

    Add files via upload
    Find the Greatest numbers

---
+++ find the three greatest number.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+   int n1,n2,n3;
+   scanf("%d %d %d",&n1,&n2,&n3);
+   if(n1>=n2 && n2>=n3)
+   {
+       printf("%d is a greatest number",n1);
+   }
+   else if(n2>=n1 && n2>=n3)
+   {
+       printf("%d is a greatest number",n2);
+   }
+   else
+   {
+       printf("%d is a greatest number",n3);
+   }
+    return 0;
+}

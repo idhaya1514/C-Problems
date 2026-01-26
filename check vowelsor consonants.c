9bd8e4106c350ad03f5ba231be6fa72c2e608f68
IdhayaKumar <idhayakumar1514@gmail.com>
2026-01-26 13:41:14 +0530

    Add files via upload
     Check Vowels or Consonants

---
+++ check vowelsor consonants.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    char ch;
+    scanf("%c",&ch);
+    
+    if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
+    {
+        printf("%c is a vowels",ch);
+    }
+    else 
+    {
+        printf("%c is a consonants",ch);
+    }
+    
+    return 0;
+}

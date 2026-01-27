3bf937909045ef262e4462c8b664a6d4fde84dbf
IdhayaKumar <idhayakumar1514@gmail.com>
2026-01-28 03:12:28 +0530

    Add files via upload

---
+++ Switch_Case/Program38.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+        int type;
+        scanf("%d",&type);
+        switch(type)
+        {
+             case 1:
+                printf("Consultation_Fee -500");
+                break;
+            case 2 : 
+                printf("Consultation_Fee - 1200");
+                break;
+            default:
+                printf("Invalid Inputs");
+              } return 0;
+        
+        
+}

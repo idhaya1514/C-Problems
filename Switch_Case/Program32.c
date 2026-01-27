d2aad15c0cdc73ed74092d19bc3b2ac65442c39b
IdhayaKumar <idhayakumar1514@gmail.com>
2026-01-28 03:09:59 +0530

    Add files via upload

---
+++ Switch_Case/Program32.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+        int type;
+        scanf("%d",&type);
+        switch(type)
+        {
+             case 1:
+                printf(" Fine-1000");
+                break;
+            case 2 : 
+                printf(" Fine-1500");
+                break;
+            case 3:
+                printf(" Fine-2000");
+                break;
+            default:
+                printf("Invalid Inputs");
+              
+            
+               
+        } return 0;
+        
+        
+}

5be7454a42966a82253c13ea4e0dce3271191a7a
IdhayaKumar <idhayakumar1514@gmail.com>
2026-01-28 03:08:13 +0530

    Add files via upload

---
+++ Switch_Case/Program28.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+        int type;
+        scanf("%d",&type);
+        switch(type)
+        {
+            case 1:
+                int amt1=80000;
+                printf("%d",amt1);
+                break;
+            case 2 :
+                int amt2=60000;
+                printf("%d",amt2);
+                break;
+            case 3:
+                int amt3=45000;
+                printf("%d",amt3);
+                break;
+            
+               
+        } return 0;
+        
+        
+}

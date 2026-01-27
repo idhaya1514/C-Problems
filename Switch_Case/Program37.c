39cde48c26db2ee2f61e76dd4b1fc9fcbf45eefd
IdhayaKumar <idhayakumar1514@gmail.com>
2026-01-28 03:12:05 +0530

    Add files via upload

---
+++ Switch_Case/Program37.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+        int class;
+        scanf("%d",&class);
+        switch(class)
+        {
+             case 1:
+                printf("Certificate_Fee-0");
+                break;
+            case 2 : 
+                printf("Certificate_Fee-500");
+                break;
+            default:
+                printf("Invalid Inputs");
+              } return 0;
+        
+        
+}

20c8b6697b78430d1554564d938c968e1ef42aaf
IdhayaKumar <idhayakumar1514@gmail.com>
2026-01-28 03:09:37 +0530

    Add files via upload

---
+++ Switch_Case/Program31.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+        int month;
+        scanf("%d",&month);
+        switch(month)
+        {
+             case 1:
+                printf("Membership Fees-1500");
+                break;
+            case 2 : 
+                printf("Membership Fees-4000");
+                break;
+            case 3:
+                printf("Membership Fees-7000");
+                break;
+            default:
+                printf("Invalid Inputs");
+              
+            
+               
+        } return 0;
+        
+        
+}

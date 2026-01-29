9410b93fdb7c6ea82226916c0edef89333d20a43
IdhayaKumar <idhayakumar1514@gmail.com>
2026-01-29 23:07:58 +0530

    Add files via upload

---
+++ Nested_For_Loop/Program19.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main() {
+    int n;
+    scanf("%d",&n);
+
+    for (int i=1;i<=n;i++) {
+        for (int j=i;j<=n;j++){
+            printf("*");
+        }
+        for (int k=1;k<=2*(i-1);k++) {
+            printf(" ");
+        }
+        for (int l=i;l<=n;l++) {
+            printf("*");
+        }
+    printf("\n");
+    }
+
+    for (int i=n;i>=1;i--) {
+        for (int j=i;j<=n;j++) {
+            printf("*");
+        }
+        for (int k=1;k<=2*(i-1);k++) {
+            printf(" ");
+
+        }
+        for (int l=i;l<=n;l++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
